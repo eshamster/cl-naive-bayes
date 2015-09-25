f1193c3b2c8273812dabe81340608cf62af8b3e7
eshamster <hamgoostar@gmail.com>
2015-09-25 15:10:33 +0900

    Be implementing Naive Bayes by the naive list
    	modified:   cl-naive-bayes.asd
    	modified:   src/cl-naive-bayes.lisp

--- src/cl-naive-bayes.lisp
+++ src/cl-naive-bayes.lisp
@@ -1,6 +1,41 @@
 (in-package :cl-user)
 (defpackage cl-naive-bayes
-  (:use :cl))
+  (:use :cl)
+  (:import-from :anaphora
+                :aif
+                :it))
 (in-package :cl-naive-bayes)
 
-;; blah blah blah.
+(defstruct word-category-count
+  word
+  category
+  (count 0))
+
+(defstruct learned-store
+  lst
+  (num-word-kind 0))
+
+(defun count-word-in-category (store word category)
+  (aif (find-if #'(lambda (elem)
+                    (and (equal (word-category-count-word elem) word)
+                         (equal (word-category-count-category elem) category)))
+                (learned-store-lst store))
+       (word-category-count-count it)
+       0))
+
+(defun count-category (store category)
+  (loop for elem in (learned-store-lst store)
+     when (equal (word-category-count-category elem) category)
+     sum (word-category-count-count elem)))
+
+(defun count-word-kind (store)
+  (learned-store-num-word-kind store))
+
+; using Laplace Smoothing
+(defun get-word-logged-likelihood (store word-lst category)
+  (let ((denomi (max (+ (count-category store category)
+                        (count-word-kind store))
+                     1))
+        (logged-numer (loop for word in word-lst
+                         sum (log (1+ (count-word-in-category store word category))))))
+    (- logged-numer (log denomi))))
